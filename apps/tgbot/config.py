060723c74f663e4553729c8cc8770a2fcb30d9f0
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-06-03 12:07:29 +0500

    Updated

---
+++ apps/tgbot/config.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class DbConfig:
+    host: str
+    password: str
+    user: str
+    database: str
+
+
+@dataclass
+class TgBot:
+    token: str
+    admin_ids: list[int]
+    use_mongo: bool
+    webhook: bool
+    webhook_url: str
+
+
+@dataclass
+class Miscellaneous:
+    other_params: str = None
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: DbConfig
+    misc: Miscellaneous
+
+
+def load_config(path: str = None):
+    env = Env()
+    env.read_env(path)
+
+    return Config(
+        tg_bot=TgBot(
+            token=env.str("BOT_TOKEN"),
+            admin_ids=list(map(int, env.list("ADMINS"))),
+            use_mongo=env.bool("USE_MONGO", default=False),
+            webhook=env.bool("USE_WEBHOOK", default=False),
+            webhook_url=env.str("WEBHOOK_URL", default=""),
+        ),
+        db=DbConfig(
+            host=env.str('DB_HOST'),
+            password=env.str('DB_PASS'),
+            user=env.str('DB_USER'),
+            database=env.str('DB_NAME')
+        ),
+        misc=Miscellaneous()
+    )
